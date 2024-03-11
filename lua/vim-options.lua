00b4427afda5c92464661cf860818fe1ce1703b2
AndryianauAliaksei <aandryianau@assurance.com>
2024-03-11 19:39:50 +0100

    lil fixes

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set list")
+vim.cmd("set lcs+=space:·")
 vim.g.mapleader = " "
 vim.wo.relativenumber = true
 
