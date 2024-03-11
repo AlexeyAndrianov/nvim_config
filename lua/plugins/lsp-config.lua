b94e3f47576a339354680aca478800e0863ba2c9
AndryianauAliaksei <aandryianau@assurance.com>
2024-03-11 10:24:29 +0100

    made ruby autoformat

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ruby_ls", "rubocop"}
+        ensure_installed = { "lua_ls", "ruby_ls" }
       })
     end
   },
@@ -19,7 +19,6 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.ruby_ls.setup({})
-      lspconfig.rubocop.setup({})
 
       vim.keymap.set('n', 'E', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
--- lua/qwe.rb
+++ lua/qwe.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
+# comment
+class ABS
+  def qwe2
+    puts 'qwe2'
+  end
+end
+
 def qwe
   puts 'qwe'
 end
 
 qwe
+
+qwe2
